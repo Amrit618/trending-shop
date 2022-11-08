4fb1926b5f6ebd936cd599011dab90003f3df792
Amrit Bastakoti <amritbastakoti618@gmail.com>
2022-11-08 22:11:46 +0200

    1st slide

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -27,7 +27,47 @@ const Arrow = styled.div<myProps>`
   left: ${(myProps) => myProps.direction === "left" && "10px"};
   right: ${(myProps) => myProps.direction === "right" && "10px"};
   margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
 `;
+const Wrapper = styled.div`
+  height: 100%;
+`;
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+`;
+const ImageContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+const Image = styled.img`
+  height: 80%;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1`
+font-size: 70px;
+`
+const Desc = styled.h1`
+margin: 50px 0px;
+font-size:20px;
+font-weight:500;
+letter-spacing: 3px;
+`
+const Button = styled.button`
+padding:10px;
+font-size: 20px;
+background-color: transparent;
+cursor: pointer;
+
+`
 
 const Slider = () => {
   return (
@@ -35,6 +75,18 @@ const Slider = () => {
       <Arrow direction="left">
         <ArrowLeftOutlined />
       </Arrow>
+      <Wrapper>
+        <Slide>
+          <ImageContainer>
+            <Image src="https://cdn.pixabay.com/photo/2017/02/26/02/37/girl-2099359_1280.jpg" />
+          </ImageContainer>
+          <InfoContainer>
+            <Title>WINTER SALE</Title>
+            <Desc>GET 40% DISCOUNT </Desc>
+            <Button>SHOP NOW</Button>
+          </InfoContainer>
+        </Slide>
+      </Wrapper>
       <Arrow direction="right">
         <ArrowRightOutlined />
       </Arrow>
