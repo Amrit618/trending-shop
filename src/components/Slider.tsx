b67d50cdd046f47c2271ae13e7763087a2bd1275
Amrit Bastakoti <amritbastakoti618@gmail.com>
2022-11-08 21:34:44 +0200

    preetified code

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,46 +1,45 @@
-import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons"
-import styled from "styled-components"
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
+import styled from "styled-components";
 
 interface myProps {
-    direction: string;
+  direction: string;
 }
 
 const Container = styled.div`
-width: 100%;
-height:100vh;
-display: flex;
-background-color:coral;
-position: reltive;
-`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  background-color: coral;
+  position: reltive;
+`;
 
 const Arrow = styled.div<myProps>`
-width:50px;
-height: 50px;
-background-color: #fff7f7;
-border-radius: 50%;
-display:flex;
-align-items: center;
-justify-content:center;
-position: absolute;
-top:0;
-bottom:0;
-left:${myProps=>myProps.direction=== 'left' && '10px'};
-right:${myProps=>myProps.direction=== 'right' && '10px'};
-margin:auto; 
-
-`
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(myProps) => myProps.direction === "left" && "10px"};
+  right: ${(myProps) => myProps.direction === "right" && "10px"};
+  margin: auto;
+`;
 
 const Slider = () => {
   return (
     <Container>
-        <Arrow direction='left'>
-            <ArrowLeftOutlined/>
-        </Arrow>
-        <Arrow direction = 'right'>
-            <ArrowRightOutlined/>
-        </Arrow>
+      <Arrow direction="left">
+        <ArrowLeftOutlined />
+      </Arrow>
+      <Arrow direction="right">
+        <ArrowRightOutlined />
+      </Arrow>
     </Container>
-  )
-}
+  );
+};
 
-export default Slider
+export default Slider;
