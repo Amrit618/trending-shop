fe202f8962f963701c4c8b4a286189855bb8810f
Amrit Bastakoti <amritbastakoti618@gmail.com>
2022-11-08 11:53:18 +0200

    added styled components

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-  Hello World
+  <Home />
     </div>
   );
 }
