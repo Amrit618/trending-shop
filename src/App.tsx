3fdcbd076c2917a16359ad65d0aa43b64319e490
Amrit Bastakoti <amritbastakoti618@gmail.com>
2022-11-14 21:26:49 +0200

    products added to productlist page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import logo from "./logo.svg";
 import { Counter } from "./features/counter/Counter";
 import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <ProductList />
     </div>
   );
 }
--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -1,12 +1,34 @@
 import styled from "styled-components"
+import Navbar from "../components/Navbar"
+import Products from "../components/Products"
+import Trending from "../components/Trending"
 
 const Container = styled.div`
 
 `
+const Title = styled.h1`
+margin: 20px;
+`
+const FilterContainer = styled.div`
+display: flex;
+justify-content: space-between;
+`
+const Filter = styled.div`
+margin: 20px;
+`
 
 const ProductList = () => {
   return (
-    <Container>ProductList</Container>
+    <Container>
+        <Navbar/>
+        <Trending />
+        <Title>Dresses</Title>
+        <FilterContainer>
+            <Filter>Filter1</Filter>
+            <Filter>Filter2</Filter>
+        </FilterContainer>
+        <Products />
+    </Container>
   )
 }
 
