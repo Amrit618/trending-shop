420b0be3d4f2183e00abb637a73ed6183ae56ced
Amrit Bastakoti <amritbastakoti618@gmail.com>
2022-11-15 20:05:38 +0200

    error typescript on Cart

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import styled from "styled-components";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Trending from "../components/Trending";
+
+interface props{
+    type: string;
+}
+
+const Container = styled.div``;
+
+const Wrapper = styled.div`
+  padding: 20px;
+`;
+
+const Title = styled.h1`
+  font-weight: 300;
+  text-align: center;
+`;
+
+const Top = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const TopButton = styled.button<props>`
+  padding: 10px;
+  font-weight: 600;
+  cursor: pointer;
+  border: ${props=>props.type === "filled" && "none"};
+  background-color: ${props=>props.type === "filled" ? "black" : "transparent"};
+  color: ${props=>props.type === "filled" && "white"};
+`;
+
+const TopTexts = styled.div``
+
+const TopText = styled.span`
+text-decoration: underline;
+cursor: pointer;
+margin: 0px 10px;
+`
+
+const Bottom = styled.div``;
+
 
 const Cart = () => {
   return (
-    <div>Cart</div>
-  )
-}
+    <Container>
+      <Navbar />
+      <Trending />
+      <Wrapper>
+        <Title> Your bag</Title>
+        <Top>
+          <TopButton>CONTINUE SHOPPING</TopButton>
+          <TopTexts>
+            <TopText>Shopping Bag(2)</TopText>
+            <TopText>Your Wishlist</TopText>
+          </TopTexts>
+          <TopButton type="filled">CHECKOUT</TopButton>
+        </Top>
+        <Bottom></Bottom>
+      </Wrapper>
+      <Footer />
+    </Container>
+  );
+};
 
-export default Cart
+export default Cart;
