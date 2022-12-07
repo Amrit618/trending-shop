be6b20bf8875c0050e8a5ef77700fad0c10ee2fe
Amrit Bastakoti <amritbastakoti618@gmail.com>
2022-12-07 21:18:59 +0200

    connected backend

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require ("express")
 const app = express()
 const mongoose = require('mongoose')
+
 mongoose.connect("mongodb+srv://amrit:amrit@amritcluster.ufmjl.mongodb.net/?retryWrites=true&w=majority")
 .then(()=>("DBConnection successful"))
+.catch((error) =>
+{console.log('error')});
+
 app.listen(4000, () =>{
     console.log("First program is running new on server")
 })
