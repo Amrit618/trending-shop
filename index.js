c5db1e763d51212d4376fdd4582ffaaf4bc36bcc
Amrit Bastakoti <amritbastakoti618@gmail.com>
2022-12-07 21:14:00 +0200

    new on dbconnection

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require ("express")
 const app = express()
 const mongoose = require('mongoose')
 mongoose.connect("mongodb+srv://amrit:amrit@amritcluster.ufmjl.mongodb.net/?retryWrites=true&w=majority")
+.then(()=>("DBConnection successful"))
 app.listen(4000, () =>{
     console.log("First program is running new on server")
 })
