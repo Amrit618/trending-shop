eaf6c3e18a7ac5319d551ec3112711f439e1ee98
Amrit Bastakoti <amritbastakoti618@gmail.com>
2022-12-10 20:36:45 +0200

    preetified

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const express = require ("express")
-const app = express()
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-dotenv.config()
-mongoose.set('strictQuery', true);
+dotenv.config();
+mongoose.set("strictQuery", true);
 
- mongoose
-.connect(process.env.MONGO_URI)
-.then(()=>console.log("DBConnection successful"))
-.catch((error) =>
-{
-    console.log('error')
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DBConnection successful"))
+  .catch((error) => {
+    console.log("error");
+  });
+
+app.listen(process.env.PORT || 4000, () => {
+  console.log("First program is running new on server on port 4000");
 });
-
-app.listen(process.env.PORT || 4000, () =>{
-    console.log("First program is running new on server on port 4000")
-})
