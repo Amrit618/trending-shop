c1024f4a767d9845314df07356921abeca3ca933
Amrit Bastakoti <amritbastakoti618@gmail.com>
2022-12-12 21:46:14 +0200

    mongodb set

--- index.js
+++ index.js
@@ -2,20 +2,19 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const userRoute = require("./routes/User")
-
+const userRoute = require("./routes/User");
 
 dotenv.config();
 mongoose
-.connect("mongodb+srv://amrit:amrit@amritcluster.ufmjl.mongodb.net/?retryWrites=true&w=majority")
-.then(() => console.log("DB successful"))
-.catch((err) =>{
+  .connect(
+    "mongodb+srv://amrit:amrit@amritcluster.ufmjl.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("DB successful"))
+  .catch((err) => {
     console.log(err);
-})
-
-app.use("/api", userRoute)
-
-
+  });
+app.use(express.json())
+app.use("/api", userRoute);
 
 app.listen(4000, () => {
   console.log("Backend server is running");
