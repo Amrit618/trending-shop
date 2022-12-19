161127cf30469a5a0ac36ff86953f1e57b79ec78
Amrit Bastakoti <amritbastakoti618@gmail.com>
2022-12-19 20:54:52 +0200

    new changes on product model

--- model/Product.js
+++ model/Product.js
@@ -6,10 +6,10 @@ const ProductSchema = new mongoose.Schema(
     title: { type: String, required: true, unique: true },
     desc: { type: String, required: true, unique: true },
     img: { type: String, required: true },
-    categories: { type: String, required: true },
+    categories: { type: Array},
     size: { type: String, required: true },
     color: { type: String, required: true },
-    price: { type: String, required: true },
+    price: { type: Number, required: true },
   },
   { timestamps: true }
 );
