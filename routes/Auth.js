cacb724d6d26fed875f46ca62bbba13599287d27
Amrit Bastakoti <amritbastakoti618@gmail.com>
2023-01-02 20:33:56 +0200

    new authorisation

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,9 +1,21 @@
 const router = require("express").Router();
+const User = require("../model/User")
 
 
 //Register
-router.post("/register", (req, res) => {
-    const newUser = new User()
+router.post("/register", async (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+    })
+
+    try {
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    } catch(err) {
+        res.status(500).json(err)
+    }
 })
 
 module.exports = router;
