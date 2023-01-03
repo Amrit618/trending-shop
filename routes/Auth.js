1a4c33139790982e83fd95f788610bbb950113b1
Amrit Bastakoti <amritbastakoti618@gmail.com>
2023-01-03 20:30:02 +0200

    register user

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,21 +1,20 @@
 const router = require("express").Router();
-const User = require("../model/User")
-
+const User = require("../model/User");
 
 //Register
 router.post("/register", async (req, res) => {
-    const newUser = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-    })
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  });
 
-    try {
-        const savedUser = await newUser.save();
-        res.status(201).json(savedUser);
-    } catch(err) {
-        res.status(500).json(err)
-    }
-})
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
