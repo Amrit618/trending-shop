6e3cd12b12d0d347f4712e4cdecedd1d384b4f42
Amrit Bastakoti <amritbastakoti618@gmail.com>
2023-01-07 23:55:24 +0200

    authorization added

--- routes/User.js
+++ routes/User.js
@@ -1,4 +1,9 @@
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+
 const router = require("express").Router();
+router.put("/:id",verifyTokenAndAuthorization, (req,res) =>{
+
+})
 
 
 module.exports = router;
