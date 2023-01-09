6e72d2d6ea2c6f07ad6aa4cab3ff63199f52f3cc
Amrit Bastakoti <amritbastakoti618@gmail.com>
2023-01-09 21:09:19 +0200

    authorized users

--- routes/User.js
+++ routes/User.js
@@ -1,5 +1,5 @@
 const User = require("../model/User");
-const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
 
 const router = require("express").Router();
 router.put("/:id",verifyTokenAndAuthorization, async (req,res) =>{
@@ -19,6 +19,24 @@ router.put("/:id",verifyTokenAndAuthorization, async (req,res) =>{
         res.status(500).json(err)
     }
 })
+//DELETE
+router.delete("/:id", verifyTokenAndAuthorization, async(req,res)=>{
+  try{
+    await User.findByIdAndDelete(req.params.id)
+    res.status(200).json("User deleted successfully")
+  } catch (err){
+    res.status(500).json(err)
+  }
+})
 
-
+//GET USER
+router.get("/:id", verifyTokenAndAdmin, async(req,res)=>{
+  try{
+    const user = await User.findById(req.params.id)
+    const {password, ...others} = user._doc
+    res.status(200).json(others)
+  } catch (err){
+    res.status(500).json(err)
+  }
+})
 module.exports = router;
